7dda7882770432f71539ec12cda78dd1fb31ebdb
emilaa <emilaa@code.edu.az>
2022-12-09 21:19:59 +0400

    Created About and Contact page

---
+++ Online Shop - FrontEnd/assets/js/about.js
@@ -0,0 +1,13 @@
+"use strict";
+$(function () {
+  $("#product-slider-2").owlCarousel({
+    items: 4,
+    itemsDestop: [1199, 2],
+    itemsDestopSmall: [980, 2],
+    itemsMoile: [700, 1],
+    pagination: false,
+    navigation: true,
+    navigationText: ["", ""],
+    autoPlay: true,
+  });
+});

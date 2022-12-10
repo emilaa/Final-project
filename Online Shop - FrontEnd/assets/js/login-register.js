d250cf3fe1c9808eba9cecd86364ac6bf57ab140
emilaa <emilaa@code.edu.az>
2022-12-10 15:08:14 +0400

    Created Login-Register page

--- Online Shop - FrontEnd/assets/js/login-register.js
+++
@@ -1,8 +0,0 @@
-let area = document.querySelector("#login-register .col-12");
-let click = document.querySelectorAll(".login-form a");
-
-function toggleForm() {
-  area.classList.toggle("active");
-}
-
-click.addEventListener("click", toggleForm())

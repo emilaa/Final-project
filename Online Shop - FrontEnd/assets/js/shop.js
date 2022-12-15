3daa2c20e0c375e29c11927cee0163f56ff17986
emilaa <emilaa@code.edu.az>
2022-12-16 02:55:15 +0400

    I changed tab-menu for products in Shop page

--- Online Shop - FrontEnd/assets/js/shop.js
+++ Online Shop - FrontEnd/assets/js/shop.js
@@ -17,6 +17,21 @@ $(function () {
       $("div").removeClass("inActive");
     }
   }
+
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post-box").show("1000");
+    }
+    else{
+      $(".post-box").not("." + value).hide("1000");
+      $(".post-box").filter("." + value).show("1000");
+    }
+  });
+
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
 });
 
 const rangeInput = document.querySelectorAll(".range-input input"),
